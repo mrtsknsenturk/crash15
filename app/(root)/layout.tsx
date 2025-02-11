5a70a8e971d31a6cc2952fd199b97da9e3caae49
swiftadminn <swiftdeveloperr@gmail.com>
2025-02-12 00:54:40 +0300

    sayfalar eklendi github oturum acma ozelligi eklendi1

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import {ReactNode} from "react";
+import Navbar from "@/app/components/Navbar";
+
+export default function ({children}:Readonly<{ children: ReactNode }>) {
+    return (
+        <main className={"font-work-sans"}>
+            <Navbar/>
+            {children}
+        </main>
+    )
+}
